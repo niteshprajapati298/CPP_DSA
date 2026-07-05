788a3da6d97bb5caa1d3982f2a1548846766b4e9
NiteshPrajapati <niteshkmrprajapati298@gmail.com>
2026-07-05 23:02:03 +0530

    Add Fibonacci sequence implementation using recursion

---
+++ DSA/Recursion/fibonacci.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int fib(int n ){
+    if(n==0){
+        return 0;
+}
+    if(n==1){
+        return 1;
+    }
+    return fib(n-1) + fib(n-2);
+}
+int main(){
+    int n = 10;
+    cout << "Fibbonaci of " << n << " is " << fib(20) << endl;
+    return 0;
+}
