61f36d760f41f8ad2c01e877ff0ccc1fbf2eb019
NiteshPrajapati <niteshkmrprajapati298@gmail.com>
2026-08-10 22:43:15 +0530

    feat: implement strategy pattern for robot behaviors with walk, talk, and fly functionalities

--- SystemDesign/StrategyDesignPattern/class1.cpp
+++ SystemDesign/StrategyDesignPattern/class1.cpp
@@ -1,21 +1,105 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
-class Robot {
-   
-};
-class WalkableRobot {
-    virtual void walk()=0;
+
+// Strategy Interface for Walk 
+class WalkableRobot
+{
+    public :
+    virtual void walk() = 0;
     virtual ~WalkableRobot() {}
 };
-class TalkableRobot {
+class NormalWalk : public WalkableRobot
+{
+    public :
+    void walk() override
+    {
+        cout << "Normal Walking..." << endl;
+    }
+};
+
+class NoWalk : public WalkableRobot
+{
+    public :
+    void walk() override
+    {
+        cout << "Cannot Walk..." << endl;
+    }
+};
+
+// Strategy Interface for Talk 
+class TalkableRobot
+{
+    public :
+    virtual void talk() = 0;
+    virtual ~TalkableRobot() {};
+};
+
+// Concrete Stratigies for walk
+class NormalTalk : public TalkableRobot
+{
+    public :
+    void talk() override
+    {
+        cout << "Normal Talking...." << endl;
+    };
+};
+class NoTalk : public TalkableRobot
+{   
+    public :
+    void talk() override
+    {
+        cout << "Cannot Talk...." << endl;
+    };
+};
+
+// Strategy Interface for Fly
+class FlyableRobot
+{  
+    public :
+    virtual void fly() = 0;
+    virtual ~FlyableRobot() {};
+};
+class NormalFly : public FlyableRobot
+{  
+    public :
+    void fly() override
+    {
+        cout << "Normally Flying..." << endl;
+    };
+};
+class NoFly : public FlyableRobot
+{    public :
+    void fly() override
+    {
+        cout << "Cannot Fly" << endl;
+    };
+};
+
+class Robot {
+    protected :
+    WalkableRobot *walkBehaviour;
+    TalkableRobot *talkBehaviour;
+    FlyableRobot *flyBehaviour;
+    public :
+    Robot(WalkableRobot* w , TalkableRobot* t , FlyableRobot* f){
+      this->flyBehaviour = f;
+      this->talkBehaviour = t;
+      this->walkBehaviour = w;
+    };
+    void walk(){
+        walkBehaviour->walk();
+    }
+    void talk(){
+        talkBehaviour->talk();
+    }
+    void fly(){
+        flyBehaviour->fly();
+    }
 
 };
-class FlyableRobot {
 
-};
-
-
-int main(){
+int main()
+{
 
 }
